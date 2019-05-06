26fc971f2374e39dd9d3920d6b5f2ac204b1d272
novak <kelvin.onkundi@andela.com>
2019-05-06 18:35:50 +0300

    Updates

---
+++ precache-manifest.76ef006e225478c57834778061e73042.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "889783b0f096a41e1dcc82a870c5bfbf",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "4a65ed0288d590f1e510",
+    "url": "/contactmanager/static/css/2.b9dc5a1e.chunk.css"
+  },
+  {
+    "revision": "c43acce8551e519edaab",
+    "url": "/contactmanager/static/css/main.7688cc89.chunk.css"
+  },
+  {
+    "revision": "4a65ed0288d590f1e510",
+    "url": "/contactmanager/static/js/2.a3f8f682.chunk.js"
+  },
+  {
+    "revision": "c43acce8551e519edaab",
+    "url": "/contactmanager/static/js/main.1ca44111.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
